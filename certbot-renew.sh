7f9990f2de5f10e89350c308fb2ed83aae08c739
inlee <einable@gmail.com>
2020-03-04 20:30:40 +0900

    Update certbot-renew.sh

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -2,5 +2,8 @@
 
 SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
 
-sudo docker-compose -f $SCRIPTPATH/docker-compose-certbot.yml run --rm certbot-renew
+sudo docker-compose -f $SCRIPTPATH/docker-compose-certbot.yml \
+                    --env-file $SCRIPTPATH/conf/.env \
+                    run --rm certbot-renew
+
 sudo docker-compose restart nginx-proxy
