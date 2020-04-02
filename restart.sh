65f3cdaa4b6c8c43bf87972a598184c4281d6889
inlee <einable@gmail.com>
2020-04-03 02:30:34 +0900

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -3,5 +3,5 @@
 # 5. Create from http + https configuration to default.template.
 cat ./conf.d/nginx/http.template ./conf.d/nginx/https.template | sudo tee ./conf.d/nginx/default.template
 
-# 6. Restart reverse proxy container.
-sudo docker-compose restart nginx-proxy
+# 3. Run Server
+sudo docker-compose up -d
