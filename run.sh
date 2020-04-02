7e8dd12c24b7aaf1d62673fa0ef7d5cb1af873e2
inlee <einable@gmail.com>
2020-04-03 02:23:38 +0900

    Modify run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,19 @@
 
 # 1. Get dhparam.
 sudo openssl dhparam -out ./conf.d/nginx/dhparam.pem 2048
-# sudo curl https://ssl-config.mozilla.org/ffdhe2048.txt | sudo tee -a ./conf.d/nginx/dhparam.pem
+# sudo curl https://ssl-config.mozilla.org/ffdhe2048.txt | sudo tee ./conf.d/nginx/dhparam.pem
 
-# 2. Run server.
+# 2. Create http configuration to default.template
+cat ./conf.d/nginx/http.template | sudo tee ./conf.d/nginx/default.template
+
+# 3. Run Server
 sudo docker-compose up -d
 
-# 3. Get certificates.
+# 4. Get certificates.
 sudo docker-compose -f docker-compose-certbot.yml run --rm certbot-certonly
 
-# 4. Append https configuration to mysite.conf.template.
-cat ./conf.d/nginx/https.template | sudo tee -a ./conf.d/nginx/mysite.template
+# 5. Create from http + https configuration to default.template.
+cat ./conf.d/nginx/http.template ./conf.d/nginx/https.template | sudo tee ./conf.d/nginx/default.template
 
-# 5. Restart reverse proxy container.
+# 6. Restart reverse proxy container.
 sudo docker-compose restart nginx-proxy
